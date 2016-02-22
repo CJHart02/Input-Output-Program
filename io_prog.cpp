0075cbecb42747e1cf4c7bb510bc93ac31301cb3
CJHart02 <carlton.hart@gmail.com>
2016-02-21 20:30:25 -0500

    Update io_prog.cpp

--- io_prog.cpp
+++ io_prog.cpp
@@ -1,3 +1,8 @@
+// program 1 Carlton Hart cjhart
+// input data from file and output to different file
+// due 2-22-2016
+// [enter relevant data here]
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -7,18 +12,31 @@ using namespace std;
 
 int main()
 {
-    string firstName, lastName, department
-    double grossSalary, bonus, taxes;
-    double distance, travelTime;
-    double cupsSold, cupCost;
-
-    ifstream inData;
-    ofstream outData;
-
-    inData.open("inData.txt")
-    outData.open("outData.txt")
-
-    inData >> firstName >> lastName >> department >> grossSalary >> bonus >> taxes >> distance >> travelTime >> cupsSold >> cupCost;
+        string firstName, lastName, department;
+        double grossSalary, bonus, taxes, paycheck;
+        double distance, travelTime, averageSpeed;
+        double cupsSold, cupCost, salesAmount;
 
 
-    return 0;
+        ifstream inData;
+        ofstream outData;
+
+        inData.open("inData.txt");
+        outData.open("outData.txt");
+
+        inData >> firstName >> lastName >> department >> grossSalary >> bonus >> taxes >> distance >> t$
+
+        paycheck = (grossSalary + (grossSalary * (bonus/100))) - ((grossSalary + (grossSalary * (bonus/$
+        averageSpeed = distance / travelTime;
+        salesAmount = cupsSold * cupCost;
+
+        outData << "Name: " << firstName << " " << lastName << ", Department: " << department << endl;
+        outData << setprecision (2) << fixed << "Monthly Gross Salary: $" << grossSalary << ", Monthly $
+        outData << "Paycheck: $" << paycheck <<  endl << endl;
+        outData << "Distance Traveled: " << distance << " miles, Traveling Time: " << travelTime << " h$
+        outData << "Average Speed: " << averageSpeed << " miles per hour" << endl << endl;
+        outData << "Number of Coffee Cups Sold: " << cupsSold << ", Cost: $" << cupCost << " per cup" <$
+        outData << "Sales Amount = $" << salesAmount << endl;
+
+        return 0;
+}
